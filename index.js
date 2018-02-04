2e6af6f9ab25abf7957e32f2f42819fecd2ffff4
snutiise <snutiise@hanmail.net>
2018-02-05 01:27:10 +0900

    cors allow add

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 app.use('/', express.static(__dirname + '/build'));
-
+io.origins('*:*');
 io.sockets.on('connection', function (socket) {
     socket.on('channelJoin',function(channel){
         socket.join(channel);
